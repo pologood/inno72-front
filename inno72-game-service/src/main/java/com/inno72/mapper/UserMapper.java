28e93df15abbb954978ed48a3e78600a4e7aa764
zb.zhou <qwer1234>
2018-06-27 15:01:32 +0800

    删除class

--- inno72-game-service/src/main/java/com/inno72/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.inno72.mapper;
-
-import com.inno72.common.Mapper;
-import com.inno72.model.User;
-
-public interface UserMapper extends Mapper<User> {
-}
--- inno72-game-service/src/main/java/com/inno72/service/UserInfoService.java
+++
@@ -1,8 +0,0 @@
-package com.inno72.service;
-
-import com.inno72.common.Service;
-import com.inno72.model.UserInfo;
-
-public interface UserInfoService  extends Service<UserInfo>{
-	
-}
