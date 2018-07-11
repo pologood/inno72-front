80cd68a4c137aeae9b490590f3909a92cc1432da
zb.zhou <qwer1234>
2018-07-11 10:30:35 +0800

    修改JSONl引用

--- inno72-game-service/src/main/java/com/inno72/feign/MachineBackgroundFeignClient.java
+++ inno72-game-service/src/main/java/com/inno72/feign/MachineBackgroundFeignClient.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.inno72.common.Result;
 import com.inno72.model.Inno72SupplyChannel;
 
-@FeignClient(value = "machine-backend", url = "http://172.16.19.218:8880")
+//@FeignClient(value = "machine-backend", url = "http://172.16.19.218:8880")
+@FeignClient(value = "machineBackend")
 @SuppressWarnings("rawtypes")
 public interface MachineBackgroundFeignClient {
 
