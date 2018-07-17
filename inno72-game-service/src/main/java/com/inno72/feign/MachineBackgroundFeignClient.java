f3f44e95426c997d9ccd2af0318df2f6dd284b27
zb.zhou <qwer1234>
2018-07-17 19:26:19 +0800

    修改feign地址

--- inno72-game-service/src/main/java/com/inno72/feign/MachineBackgroundFeignClient.java
+++ inno72-game-service/src/main/java/com/inno72/feign/MachineBackgroundFeignClient.java
@@ -8,7 +8,7 @@ import com.inno72.common.Result;
 import com.inno72.model.Inno72SupplyChannel;
 
 //@FeignClient(value = "machine-backend", url = "http://172.16.19.218:8880")
-@FeignClient(value = "machineBackend")
+@FeignClient(value = "MACHINEBACKEND")
 @SuppressWarnings("rawtypes")
 public interface MachineBackgroundFeignClient {
 
