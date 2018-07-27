aa27943458c32c9849be7e2070581f33eaec2fad
zb.zhou <qwer1234>
2018-07-27 16:22:52 +0800

    添加日志

--- inno72-game-service/src/main/java/com/inno72/service/impl/Inno72MachineServiceImpl.java
+++ inno72-game-service/src/main/java/com/inno72/service/impl/Inno72MachineServiceImpl.java
@@ -70,10 +70,10 @@ public class Inno72MachineServiceImpl extends AbstractService<Inno72Machine> imp
 
 		if ( StringUtil.isNotBlank(planId) ){
 			String rVoJson = redisUtil.get(CommonBean.REDIS_ACTIVITY_PLAN_CACHE_KEY + planId);
-			LOGGER.info("redis cache machine data =====> {}", rVoJson);
+			LOGGER.debug("redis cache machine data =====> {}", rVoJson);
 			if ( StringUtil.isNotEmpty(rVoJson) ){
 				inno72MachineVo = JSON.parseObject(rVoJson, Inno72MachineVo.class);
-				LOGGER.info("parse rVoJson string finish --> {}", inno72MachineVo);
+				LOGGER.debug("parse rVoJson string finish --> {}", inno72MachineVo);
 			}
 		}
 
@@ -89,22 +89,22 @@ public class Inno72MachineServiceImpl extends AbstractService<Inno72Machine> imp
 			List<Inno72ActivityPlan> inno72ActivityPlans = inno72ActivityPlanMapper.selectByMachineId(inno72Machine.getId());
 
 			if (inno72ActivityPlans.size() == 0) {
-				LOGGER.info("机器 【{}】 没有配置 活动计划!", inno72Machine.getMachineCode());
+				LOGGER.debug("机器 【{}】 没有配置 活动计划!", inno72Machine.getMachineCode());
 				return Results.failure("无活动计划!");
 			}
 
 			Inno72ActivityPlan inno72ActivityPlan = inno72ActivityPlans.get(0);
-			LOGGER.info("活动计划详情 =====> {}", JSON.toJSONString(inno72ActivityPlan));
+			LOGGER.debug("活动计划详情 =====> {}", JSON.toJSONString(inno72ActivityPlan));
 
 			String gameId = inno72ActivityPlan.getGameId();
 			if (StringUtil.isEmpty(gameId)) {
-				LOGGER.info("活动计划id 【{}】 没有配置 游戏!", inno72ActivityPlan.getId());
+				LOGGER.debug("活动计划id 【{}】 没有配置 游戏!", inno72ActivityPlan.getId());
 				return Results.failure("无配置游戏!");
 			}
 
 			Inno72Game inno72Game = inno72GameMapper.selectByPrimaryKey(gameId);
 			if (inno72Game == null) {
-				LOGGER.warn("游戏id 【{}】 不存在!", gameId);
+				LOGGER.debug("游戏id 【{}】 不存在!", gameId);
 				return Results.failure("游戏不存在!");
 			}
 
@@ -115,7 +115,7 @@ public class Inno72MachineServiceImpl extends AbstractService<Inno72Machine> imp
 			String sellerId = inno72Activity.getSellerId();
 			Inno72Merchant inno72Merchant = inno72MerchantMapper.selectByPrimaryKey(sellerId);
 			if (inno72Merchant == null) {
-				LOGGER.warn("商户id 【{}】 不存在!", sellerId);
+				LOGGER.debug("商户id 【{}】 不存在!", sellerId);
 				return Results.failure("商户配置错误!");
 			}
 			inno72MachineVo.setChannelId(inno72Merchant.getChannelId());
