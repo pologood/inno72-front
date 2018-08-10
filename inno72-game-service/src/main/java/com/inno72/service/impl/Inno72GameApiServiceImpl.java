34b03ea33450d608cecbe8469061599b79c4316e
zb.zhou <zhouzengbao@inno72.com>
2018-08-10 20:40:07 +0800

    添加注释

--- inno72-game-service/src/main/java/com/inno72/service/impl/Inno72GameApiServiceImpl.java
+++ inno72-game-service/src/main/java/com/inno72/service/impl/Inno72GameApiServiceImpl.java
@@ -625,7 +625,7 @@ public class Inno72GameApiServiceImpl implements Inno72GameApiService {
 			boolean result = gameSessionRedisUtil.hasKey(sessionUuid);
 			LOGGER.info("qrCode hasKey result {} ", result);
 			if (!result) {
-				qrStatus = QRSTATUS_INVALID;
+//				qrStatus = QRSTATUS_INVALID;
 				LOGGER.info("二维码已经过期");
 			} else {
 				UserSessionVo sessionStr = gameSessionRedisUtil.getSessionKey(sessionUuid);
