03896c7380bd7d8148ac6e963f38abd94fc708e0
zb.zhou <zhouzengbao@inno72.com>
2018-08-14 18:21:51 +0800

    判断活动过期，添加二维码参数

--- inno72-game-service/src/main/java/com/inno72/service/impl/Inno72MachineServiceImpl.java
+++ inno72-game-service/src/main/java/com/inno72/service/impl/Inno72MachineServiceImpl.java
@@ -83,17 +83,6 @@ public class Inno72MachineServiceImpl extends AbstractService<Inno72Machine> imp
 
 		Inno72ActivityPlan inno72ActivityPlan = inno72MachineVo.getInno72ActivityPlan();
 
-		if (inno72ActivityPlan != null){
-			LocalDateTime startTime = inno72ActivityPlan.getStartTime();
-			LocalDateTime endTime = inno72ActivityPlan.getEndTime();
-			LocalDateTime now = LocalDateTime.now();
-			if ( !startTime.isBefore(now) && !endTime.isAfter(now)){
-				LOGGER.debug("活动过期 ==>   ", JSON.toJSONString(inno72ActivityPlan));
-				inno72MachineVo.setReload(true);
-				redisUtil.del(CommonBean.REDIS_ACTIVITY_PLAN_CACHE_KEY + planId + ":" +machineId);
-			}
-		}
-
 		return Results.success(inno72MachineVo);
 	}
 
@@ -165,9 +154,23 @@ public class Inno72MachineServiceImpl extends AbstractService<Inno72Machine> imp
 					CommonBean.REDIS_ACTIVITY_PLAN_CACHE_EX_KEY,
 					JSON.toJSONString(inno72MachineVo)
 			);
+		}else{
+			Inno72ActivityPlan inno72ActivityPlan = inno72MachineVo.getInno72ActivityPlan();
+			if (inno72ActivityPlan != null){
+				LocalDateTime startTime = inno72ActivityPlan.getStartTime();
+				LocalDateTime endTime = inno72ActivityPlan.getEndTime();
+
+				LocalDateTime now = LocalDateTime.now();
+				if ( !startTime.isBefore(now) && !endTime.isAfter(now)){
+					LOGGER.info("活动过期 ==>   ", JSON.toJSONString(inno72ActivityPlan));
+					redisUtil.del(CommonBean.REDIS_ACTIVITY_PLAN_CACHE_KEY + planId + ":" +machineId);
+					return Results.warn("过期了!",2);
+				}
+			}
 		}
 		return Results.success(inno72MachineVo);
 	}
+
 
 	private Result<Inno72MachineVo> initDefaultGameFromRedis(String machineId){
 
