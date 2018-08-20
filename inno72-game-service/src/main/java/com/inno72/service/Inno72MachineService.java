df625333f680210c9200542ff0366593d0f78fec
xingang <546657868@qq.com>
2018-08-20 15:29:21 +0800

    添加第一版奇门接口

--- inno72-game-service/src/main/java/com/inno72/service/Inno72MachineService.java
+++ inno72-game-service/src/main/java/com/inno72/service/Inno72MachineService.java
@@ -1,9 +1,13 @@
 package com.inno72.service;
 
+import java.util.List;
+
 import com.inno72.common.Result;
 import com.inno72.common.Service;
 import com.inno72.model.Inno72Machine;
 import com.inno72.vo.Inno72MachineVo;
+import com.inno72.vo.MachineVo;
+import com.inno72.vo.QimenTmallFansAutomachineQureymachinesRequest;
 
 /**
  * Created by CodeGenerator on 2018/06/27.
@@ -12,4 +16,12 @@ public interface Inno72MachineService extends Service<Inno72Machine> {
 
 	Result<Inno72MachineVo> findGame(String mid, String plantId, String version, String versionInno72);
 
+	List<MachineVo> selectListByQimen(QimenTmallFansAutomachineQureymachinesRequest.MachineQuery query);
+
+	/**
+	 * tmall.fans.automachine.savemachine( 注册、更新供应商上的设备信息到天猫互动吧 )
+	 * @param machineId 监听到的ID
+	 * @return MachineVo
+	 */
+	Result<MachineVo> selectTianMaoMachineVoById(String machineId);
 }
