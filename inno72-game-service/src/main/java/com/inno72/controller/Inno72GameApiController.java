c708d76651be8eeddcd10ca0161645a51813370c
72cy-0101-01-0008 <1632647543@qq.com>
2018-08-21 14:47:31 +0800

    派样查询

--- inno72-game-service/src/main/java/com/inno72/controller/Inno72GameApiController.java
+++ inno72-game-service/src/main/java/com/inno72/controller/Inno72GameApiController.java
@@ -1,5 +1,7 @@
 package com.inno72.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inno72.common.Result;
+import com.inno72.model.Inno72Activity;
 import com.inno72.service.Inno72GameApiService;
 import com.inno72.vo.MachineApiVo;
 
@@ -140,7 +143,7 @@ public class Inno72GameApiController {
 	 * @return Result
 	 */
 	@RequestMapping(value = "/sampling", method = {RequestMethod.POST, RequestMethod.GET})
-	public Result<Object> sampling() {
+	public Result<List<Inno72Activity>> sampling() {
 		return inno72GameApiService.sampling();
 	}
 }
--- inno72-game-service/src/main/java/com/inno72/mapper/Inno72ActivityMapper.java
+++ inno72-game-service/src/main/java/com/inno72/mapper/Inno72ActivityMapper.java
@@ -1,5 +1,7 @@
 package com.inno72.mapper;
 
+import java.util.List;
+
 import com.inno72.common.Mapper;
 import com.inno72.model.Inno72Activity;
 import com.inno72.vo.FansActVo;
@@ -8,4 +10,6 @@ public interface Inno72ActivityMapper extends Mapper<Inno72Activity> {
 	Inno72Activity selectDefaultAct();
 
 	FansActVo selectTianMaoActVo(String actId);
+
+	List<Inno72Activity> selectActiveByType(Integer type);
 }
