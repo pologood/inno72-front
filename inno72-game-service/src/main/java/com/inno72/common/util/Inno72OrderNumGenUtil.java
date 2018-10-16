f11d9edcadadec44f76159f4995fc598b806fe82
xingang <546657868@qq.com>
2018-10-16 17:18:20 +0800

    修改订单号生成规则

--- inno72-game-service/src/main/java/com/inno72/common/util/Inno72OrderNumGenUtil.java
+++ inno72-game-service/src/main/java/com/inno72/common/util/Inno72OrderNumGenUtil.java
@@ -2,14 +2,30 @@ package com.inno72.common.util;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import com.inno72.common.datetime.LocalDateTimeUtil;
+import com.inno72.redis.IRedisUtil;
 
 @Component
 public class Inno72OrderNumGenUtil {
+
+	@Resource
+	private IRedisUtil $redisUtil;
+
+	private static IRedisUtil redisUtil;
+
+	@PostConstruct
+	public void init(){
+		redisUtil = $redisUtil;
+	}
 
 	/**
 	 *
@@ -22,16 +38,29 @@ public class Inno72OrderNumGenUtil {
 		assert StringUtils.isNotEmpty(channelNum);
 		assert StringUtils.isNotEmpty(lastSixMerNum);
 
+		String date = LocalDateTimeUtil.transfer(LocalDateTime.now(), DateTimeFormatter.ofPattern("yyyyMMdd"));
+
+		String orderNumKey = "inno72_common:order_num:";
+		String localOrderNumKey = orderNumKey + date;
+
+		Set<Object> smembers = redisUtil.smembers(localOrderNumKey);
+		if (smembers == null || smembers.size() == 0){
+			redisUtil.del(orderNumKey+"*");
+			smembers = new HashSet<>();
+		}
+
+		String random = (int) ((Math.random() * 9 + 1) * 100000)+"";
+		while (smembers.contains(random)){
+			random = (int) ((Math.random() * 9 + 1) * 100000)+"";
+		}
+
 		if (lastSixMerNum.length() > 6) {
 			lastSixMerNum = lastSixMerNum.substring(lastSixMerNum.length() - 6);
 		}
-		String orderNum = channelNum + lastSixMerNum;
 
-		String dateTime = LocalDateTimeUtil.transfer(LocalDateTime.now(), DateTimeFormatter.ofPattern("yyyyMMdd"));
+		redisUtil.sadd(localOrderNumKey, random+"");
 
-		Integer random = (int) ((Math.random() * 9 + 1) * 100000);
-
-		return orderNum + dateTime + random;
+		return channelNum + lastSixMerNum + date + random;
 	}
 
 }
