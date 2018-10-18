976b263470a459ea81dfff087310fd40013273cb
gaoxingang <546657868@qq.com>
2018-10-18 14:14:20 +0800

    findGoods 保留两位小数

--- inno72-game-service/src/main/java/com/inno72/controller/Inno72ActivityController.java
+++ inno72-game-service/src/main/java/com/inno72/controller/Inno72ActivityController.java
@@ -93,7 +93,7 @@ public class Inno72ActivityController {
 			gameResultVo.setBanner(inno72Goods.getBanner());
 			gameResultVo.setGoodsCount(goodsCount);
 			gameResultVo.setSpecRemark(inno72Goods.getSpecRemark());
-			gameResultVo.setPrice(inno72Goods.getPrice() != null ? inno72Goods.getPrice().setScale(1).toString() : "");
+			gameResultVo.setPrice(inno72Goods.getPrice() != null ? inno72Goods.getPrice().setScale(2).toString() : "");
 			gameResultVoList.add(gameResultVo);
 		}
 		return Results.success(gameResultVoList);
