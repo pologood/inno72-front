45184f3982d09d52cfaa0eea4bd9ac284a4eb609
xingang <546657868@qq.com>
2018-11-13 11:50:35 +0800

    修改mapping

--- inno72-merchant-service/src/main/java/com/inno72/mapper/Inno72MerchantTotalCountByDayMapper.java
+++ inno72-merchant-service/src/main/java/com/inno72/mapper/Inno72MerchantTotalCountByDayMapper.java
@@ -2,9 +2,14 @@ package com.inno72.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.inno72.common.Mapper;
 import com.inno72.model.Inno72MerchantTotalCountByDay;
 
 public interface Inno72MerchantTotalCountByDayMapper extends Mapper<Inno72MerchantTotalCountByDay> {
-	List<Inno72MerchantTotalCountByDay> selectList(String activityId, String city, String startDate, String endDate);
+	List<Inno72MerchantTotalCountByDay> selectList(@Param("activityId") String activityId,
+			@Param("city")String city,
+			@Param("startDate")String startDate,
+			@Param("endDate")String endDate);
 }
