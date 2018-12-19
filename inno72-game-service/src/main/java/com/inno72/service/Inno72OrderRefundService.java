3a1125a82c6278d83a84c12ce44285505322301c
gaoxingang <546657868@qq.com>
2018-12-19 16:24:55 +0800

    refund 基础代码

---
+++ inno72-game-service/src/main/java/com/inno72/service/Inno72OrderRefundService.java
@@ -0,0 +1,12 @@
+package com.inno72.service;
+
+
+import com.inno72.common.Service;
+import com.inno72.model.Inno72OrderRefund;
+
+/**
+ * Created by CodeGenerator on 2018/12/19.
+ */
+public interface Inno72OrderRefundService extends Service<Inno72OrderRefund> {
+
+}
