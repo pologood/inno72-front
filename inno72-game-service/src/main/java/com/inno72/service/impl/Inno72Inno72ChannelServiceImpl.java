1da1fd8cb9dbe685ebd889e9fea829e80728529b
zhanghanqing <zhanghanqing@inno72.com>
2019-01-03 14:10:37 +0800

    canOrder bug

--- inno72-game-service/src/main/java/com/inno72/service/impl/Inno72Inno72ChannelServiceImpl.java
+++ inno72-game-service/src/main/java/com/inno72/service/impl/Inno72Inno72ChannelServiceImpl.java
@@ -174,7 +174,7 @@ public class Inno72Inno72ChannelServiceImpl implements Inno72ChannelService {
         sessionVo.setGameId(gameId);
         sessionVo.setSessionUuid(sessionUuid);
         sessionVo.setActivityPlanId(interact.getId());
-        boolean canOrder = inno72AuthInfoService.findCanOrder(interact,sessionVo,userId);
+        boolean canOrder = inno72AuthInfoService.findCanOrder(interact,sessionVo,userChannel.getGameUserId());
         sessionVo.setCanOrder(canOrder);
         if(sessionVo.getGoodsType()!=null && UserSessionVo.GOODSTYPE_COUPON.compareTo(sessionVo.getGoodsType())==0){
             sessionVo.setCountGoods(true);
