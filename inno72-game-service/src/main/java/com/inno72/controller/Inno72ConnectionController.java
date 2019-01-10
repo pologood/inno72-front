c8cb2550cbf13638e89180f510016ab86e31eebd
zhanghanqing <zhanghanqing@inno72.com>
2019-01-10 15:57:15 +0800

    callback 测试

--- inno72-game-service/src/main/java/com/inno72/controller/Inno72ConnectionController.java
+++ inno72-game-service/src/main/java/com/inno72/controller/Inno72ConnectionController.java
@@ -1,9 +1,13 @@
 package com.inno72.controller;
 
 import com.inno72.common.Inno72BizException;
+import com.inno72.common.Inno72GameServiceProperties;
 import com.inno72.common.Result;
 import com.inno72.common.Results;
+import com.inno72.common.json.JsonUtil;
+import com.inno72.plugin.http.HttpClient;
 import com.inno72.service.Inno72ConnectionService;
+import com.inno72.vo.PushRequestVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api/connection")
@@ -36,7 +43,8 @@ public class Inno72ConnectionController {
             return Results.failure(e.getMessage());
         }
     }
-
+    @Autowired
+    Inno72GameServiceProperties inno72GameServiceProperties;
     /**
      * 申请退款
      */
@@ -45,7 +53,21 @@ public class Inno72ConnectionController {
     public Result<Object> callBack(String machineCode, String activityId, Integer type,Long version) {
         try{
             LOGGER.info("callBack machineCode = {},activityId={},type={},version={}",machineCode,activityId,type,version);
-            inno72ConnectionService.callBack(machineCode,activityId,type,version);
+//            inno72ConnectionService.callBack(machineCode,activityId,type,version);
+            //发送长连接
+            String pushServerUrl = inno72GameServiceProperties.get("pushServerUrl")+"/pusher/push/one";
+            PushRequestVo vo = new PushRequestVo();
+            Map map = new HashMap();
+            map.put("machineCode",machineCode);
+            map.put("activityId",activityId);
+            map.put("type",type);
+            map.put("version",version);
+            vo.setData(JsonUtil.toJson(map));
+            vo.setTargetCode(machineCode);
+            String request = JsonUtil.toJson(vo);
+            LOGGER.info("send msg url = {},data={}",pushServerUrl,request);
+            String response = HttpClient.post(pushServerUrl,request);
+            LOGGER.info("send msg response={}",response);
             return Results.success();
         }catch (Inno72BizException e){
             return Results.warn(e.getMessage(),-1);
